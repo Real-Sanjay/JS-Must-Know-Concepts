c536bc2676913fcd2bd8f9851a5d7c52a4650feb
sanjaynayak5994@gmail.com <sanjay.r@solugenix.com>
2025-05-08 13:19:25 +0530

    added more exercise in array section

--- Practise.js
+++ Practise.js
@@ -1,23 +1,5 @@
-// 🛠️ Exercises: Practice JavaScript Objects
-// 1️⃣ Create an Object Using Different Methods
-// Create an object person with name, age, and greet() method.
-// Use different methods (literal, constructor, Object.create(), class).
+const arr1 = [1, 2, 3, 4];
+const arr2 = [3, 4, 5, 6];
 
-function Person(name, age, func){
-  this.name = name;
-  this.age = age;
-  this.func = func;
-}
-
-
-const obj = new Person("chilli", 23, (name="name")=>{
-  console.log(name);
-});
-
-const obj2 = {
-  city: "nework",
-  country: "usa"
-}
-
-const newaray = Object.entries(obj2);
-console.log(newaray);
+const commonElements =  arr2.filter((item)=> arr1.indexOf(item) !== -1);
+console.log(commonElements);
