af38b64e05190aef9ba2722ac15b1ea567290093
Sanjay <Sanjaydh006@gmail.com>
2025-08-13 08:38:36 +0530

    completed till event loop

--- Practise.js
+++ Practise.js
@@ -1,12 +1,18 @@
-function outer() {
-  console.log(innerVar);
-  var innerVar = "outer var";
-
-  function inner() {
-    console.log(innerVar);
-    var innerVar = "inner var";
-  }
-
-  inner();
+function debounce(func, delay) {
+    let timeout;
+    console.log("in bounce function")
+    return function (...args) {
+        clearTimeout(timeout);
+        console.log("test")
+        timeout = setTimeout(() => {
+            func(...args)
+        }, delay);
+    };
 }
-outer();
+
+// document.querySelector("button").addEventListener("click", debounce(() => {
+//     console.log("Button Clicked!");
+// }, 500));
+ debounce(()=>{
+    console.log("called caller");
+}, 5000);
