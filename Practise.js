d27e91b1e9310cacf858a146a200ed3f1bbd1d75
Sanjay <Sanjaydh006@gmail.com>
2025-08-15 09:25:01 +0530

    "updated till IIF"

--- Practise.js
+++ Practise.js
@@ -1,18 +1,22 @@
-function debounce(func, delay) {
-    let timeout;
-    console.log("in bounce function")
-    return function (...args) {
-        clearTimeout(timeout);
-        console.log("test")
-        timeout = setTimeout(() => {
-            func(...args)
-        }, delay);
-    };
+function rateLimiter(fn, delay) {
+  let lastCall = 0;
+
+  return function (...args) {
+    const now = Date.now();
+    console.log("last call", lastCall);
+    if (now - lastCall >= delay) {
+      lastCall = now;
+      fn(...args);
+    } else {
+      console.log("Function call blocked due to rate limiting.");
+    }
+  };
 }
 
-// document.querySelector("button").addEventListener("click", debounce(() => {
-//     console.log("Button Clicked!");
-// }, 500));
- debounce(()=>{
-    console.log("called caller");
-}, 5000);
+const logMessage = rateLimiter((data) => console.log(data), 2000);
+
+logMessage("hi");
+
+setTimeout(() => logMessage("hello again"), 1000);
+
+setTimeout(() => logMessage("After 2s"), 2000);
