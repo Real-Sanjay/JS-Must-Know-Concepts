fbb6a2f1e4963a7c7a08afa927bf4159be940dd7
Sanjay <Sanjaydh006@gmail.com>
2025-08-22 09:16:39 +0530

    Added new exercises

--- Practise.js
+++ Practise.js
@@ -1,22 +1,20 @@
-function rateLimiter(fn, delay) {
-  let lastCall = 0;
+class Bank {
+    balance;
 
-  return function (...args) {
-    const now = Date.now();
-    console.log("last call", lastCall);
-    if (now - lastCall >= delay) {
-      lastCall = now;
-      fn(...args);
-    } else {
-      console.log("Function call blocked due to rate limiting.");
+    constructor(balance) {
+        this.balance = balance;
     }
-  };
+
+    deposit(balance) {
+        this.balance += balance;
+    }
+
+    getBalance() {
+        return this.balance;
+    }
 }
 
-const logMessage = rateLimiter((data) => console.log(data), 2000);
-
-logMessage("hi");
-
-setTimeout(() => logMessage("hello again"), 1000);
-
-setTimeout(() => logMessage("After 2s"), 2000);
+const account = new Bank(1000);
+account.deposit(100);
+console.log(account.getBalance());
+console.log(account.balance);
